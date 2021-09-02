abfaba742487cd15424ed6f701f8a6e9c1df11ca
parix1999 <parisottoedoardo@gmail.com>
2021-09-02 18:08:40 +0200

    footer

---
+++ resources/views/templetes/footer.blade.php
@@ -0,0 +1,37 @@
+<div class="jumbo-image">
+    <div class="container">
+        <div class="box-flex">
+            <div class="sx"> <!-- TO DO CAMBIARE IL NOME ALLE CLASSI SX E DX -->
+                <div class="text-sopra">
+                    <div class="dc-comics-shop">
+                        <ul>
+                            <li><a href="#"><b>DC COMICS</b></a></li>
+                            <li><a href="#">character</a></li>
+                            <li><a href="#">Comics</a></li>
+                            <li><a href="#">Movies</a></li>
+                            <li><a href="#">TV</a></li>
+                            <li><a href="#">Games</a></li>
+                            <li><a href="#">Videos</a></li>
+                            <li><a href="#">News</a></li>
+                            <li><a href="#"><b>SHOP</b></a></li>
+                            <li><a href="#">Shop DC</a></li>
+                            <li><a href="#">Shop DC collectionables</a></li>
+                        </ul>
+                    </div>
+                    <div class="dc">
+
+                    </div>
+                    <div class="sites">
+
+                    </div>
+                </div>
+                <div class="text-sotto">
+
+                </div>
+            </div>
+            <div class="dx">
+
+            </div>
+        </div>
+    </div>
+</div>
