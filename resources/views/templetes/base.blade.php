412048f290675d870986901fb67345b437e6112f
parix1999 <parisottoedoardo@gmail.com>
2021-09-02 17:02:34 +0200

    header-jumbo

--- resources/views/templetes/base.blade.php
+++ resources/views/templetes/base.blade.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Comics - @yield('title')</title>
     <link rel="stylesheet" href="{{ mix('/css/app.css') }}">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css" rel="stylesheet">
 </head>
 <body>
     
--- resources/views/templetes/header.blade.php
+++ resources/views/templetes/header.blade.php
@@ -13,8 +13,33 @@
 
 <div class="container">
     <nav>
-        <div class="box-logo">
-            <img src="" alt="">
+        <div class="box-flex">
+            <div class="box-logo">
+                <img src="/images/dc-logo.png" alt="logo">
+            </div>
+            <div class="options">
+                <ul>
+                    <li><a href="#">CHARACTERS</a></li>
+                    <li><a href="#">COMICS</a></li>
+                    <li><a href="#">TV</a></li>
+                    <li><a href="#">GAMES</a></li>
+                    <li><a href="#">COLLECTIBLES</a></li>
+                    <li><a href="#">VIDEOS</a></li>
+                    <li><a href="#">FANS</a></li>
+                    <li><a href="#">NEWS</a></li>
+                    <li><a href="#">SHOP</a></li> <!-- ICON TO DO -->
+                </ul>
+            </div>
+            <div class="ricerca">
+                <input type="text" placeholder="search"><i class="fas fa-search"></i>
+            </div>
         </div>
     </nav>
+</div>
+
+<!-- jumbo -->
+<div class="jumbo">
+    <div class="box-jumbo">
+        
+    </div>
 </div>
