be5a0087434a430ba60b77116a169b624d280a4b
parix1999 <parisottoedoardo@gmail.com>
2021-09-02 18:20:17 +0200

    footer

--- resources/views/templetes/footer.blade.php
+++ resources/views/templetes/footer.blade.php
@@ -13,16 +13,36 @@
                             <li><a href="#">Games</a></li>
                             <li><a href="#">Videos</a></li>
                             <li><a href="#">News</a></li>
-                            <li><a href="#"><b>SHOP</b></a></li>
+                            <li class="shop"><a href="#"><b>SHOP</b></a></li>
                             <li><a href="#">Shop DC</a></li>
                             <li><a href="#">Shop DC collectionables</a></li>
                         </ul>
                     </div>
                     <div class="dc">
-
+                    <ul>
+                            <li><a href="#"><b>DC</b></a></li>
+                            <li><a href="#">Terms Of Use</a></li>
+                            <li><a href="#">Privacy policy(News)</a></li>
+                            <li><a href="#">Ad Choices</a></li>
+                            <li><a href="#">Advertisinf</a></li>
+                            <li><a href="#">Jobs</a></li>
+                            <li><a href="#">Subscriptions</a></li>
+                            <li><a href="#">Talent Workships</a></li>
+                            <li><a href="#">CPSC Certificate</a></li>
+                            <li><a href="#">Rating</a></li>
+                            <li><a href="#">Shop Help</a></li>
+                            <li><a href="#">Contact Us</a></li>
+                        </ul>
                     </div>
                     <div class="sites">
-
+                    <ul>
+                            <li><a href="#"><b>SITES</b></a></li>
+                            <li><a href="#">DC</a></li>
+                            <li><a href="#">MAD Magazine</a></li>
+                            <li><a href="#">DC Kids</a></li>
+                            <li><a href="#">DC Universe</a></li>
+                            <li><a href="#">DC Power Visa</a></li>
+                        </ul>
                     </div>
                 </div>
                 <div class="text-sotto">
